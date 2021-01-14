8ee147bac805135fed248e26068cebfc1c6a9d14
wcd <825342538@qq.com>
2021-01-14 14:26:30 +0800

    first commit

---
+++ pages/create.vue
@@ -0,0 +1,17 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: wcd
+ * @Date: 2021-01-14 11:53:20
+ * @LastEditors: wcd
+ * @LastEditTime: 2021-01-14 14:22:05
+-->
+<template>
+  <div>create</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
